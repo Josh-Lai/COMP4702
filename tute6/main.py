2160f2b2a58c7a7546076a2121a6d67098c080d2
Josh <josh201181@gmail.com>
2024-03-28 16:42:05 +1000

    T6:Q2

--- tute6/main.py
+++ tute6/main.py
@@ -65,17 +65,54 @@ def question2():
     for index, row in test_data.iterrows():
         # Check if the row value is equal to three change it to two if so
         if (row.iloc[-1] == 3):
-            test_data.iloc[index,-1] = 2
-    # Create Logistic Regressor 
-    print(train_data)
-    y_train = train_data.iloc[:,-1]
+            test_data.iloc[index, -1] = 2
+    # Create Logistic Regressor
+    y_train = train_data.iloc[:, -1]
     X_train = train_data.drop(train_data.columns[-1], axis=1)
-    y_test = test_data.iloc[:,-1]
+    y_test = test_data.iloc[:, -1]
     X_test = test_data.drop(test_data.columns[-1], axis=1)
     log_reg = LogisticRegression()
     log_reg.fit(X_train, y_train)
-    # Predict the test data 
+    # Predict the test data
     y_test_predict = log_reg.predict(X_test)
+
+    # evaluate and display the Confusion matrix
+    conf_mat = confusion_matrix(y_test_predict, y_test)
+    accuracy = accuracy_score(y_test_predict, y_test)
+    disp = ConfusionMatrixDisplay(
+        confusion_matrix=conf_mat, display_labels=["Normal", "Abnormal"])
+    disp.plot(cmap="Reds")
+    plt.title("Thyroid Confusion Matrix using Logistic Regressor")
+    print("Accuracy {}".format(accuracy))
+    plt.show()
+    # Use the probability functions
+    threshold = 0.05
+    prob_test = log_reg.predict_proba(X_test)
+    print(prob_test)
+    print(y_test_predict)
+    # Loop over the proabilities, if the value is greater than threshold assign to
+    y_prob_thresh = np.array([])
+    for index, row in X_test.iterrows():
+        # Inspect the probability
+        prob_1 = prob_test[index][0]
+        if (prob_1 > threshold):
+            y_prob_thresh = np.block([y_prob_thresh, 1])
+        else:
+            y_prob_thresh = np.block([y_prob_thresh, 2])
+    print(y_prob_thresh)
+
+    # Recalculate confusion matrix
+    new_conf_mat = confusion_matrix(y_prob_thresh, y_test)
+    # Display the new confusion matrix
+    disp = ConfusionMatrixDisplay(
+        confusion_matrix=new_conf_mat, display_labels=["Normal", "Abnormal"])
+    disp.plot(cmap="Reds")
+    plt.title(
+        "Thyroid Confusion Matrix with Logistic Regression (r = {})".format(threshold))
+    print("Accuracy with r = {}: {}".format(
+        threshold, accuracy_score(y_prob_thresh, y_test)))
+    plt.show()
+
 
 if __name__ == "__main__":
     # question1()
