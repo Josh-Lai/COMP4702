67ad4772312f324cf8c2444243dda205bdc3b065
Josh <josh201181@gmail.com>
2024-04-21 20:12:03 +1000

    Added torch classifier demo

--- tute7/demo.py
+++ tute7/demo.py
@@ -44,36 +44,29 @@ class Net(nn.Module):
       return x
 
 
-if __name__ == "__main__":
+def create_model(batch_size = 4):
   transform = transforms.Compose(
       [transforms.ToTensor(),
        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
       ]
   )
-
-  batch_size = 4
-
   trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                           download=True, transform=transform)
   trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=True, num_workers=2)
 
-  testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                         download=True, transform=transform)
-  testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                           shuffle=False, num_workers=2)
-
-  classes = ('plane', 'car', 'bird', 'cat',
-             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
   net = Net()
+  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+  print(device)
 
+  net.to(device)
   criterion = nn.CrossEntropyLoss()
   optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
   for epoch in range(2):
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
-      inputs, labels = data
+      inputs, labels = data[0].to(device), data[1].to(device)
       #print(inputs)
       #print(labels)
       
@@ -92,4 +85,64 @@ if __name__ == "__main__":
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
 
+  PATH = "./cifar_net.pth"
+  torch.save(net.state_dict(), PATH)
+
+if __name__ == "__main__":
+  batch_size = 4
+  #create_model(batch_size)
+  transform = transforms.Compose(
+      [transforms.ToTensor(),
+       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+      ]
+  )
+  net = Net()
+  net.load_state_dict(torch.load("./cifar_net.pth"))
+  
+  testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                         download=True, transform=transform)
+  testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                           shuffle=False, num_workers=2)
+
+  classes = ('plane', 'car', 'bird', 'cat',
+             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+  # Test model on the test data
+  dataiter = iter(testloader)
+  images, labels = next(dataiter)
+
+  imshow(torchvision.utils.make_grid(images))
+  print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+
+  correct = 0
+  total = 0
+
+  with torch.no_grad():
+    for data in testloader:
+      images, labels = data
+      outputs = net(images)
+
+      # Choose the class with the highest energy
+      _, predicted = torch.max(outputs.data, 1)
+      total += labels.size(0)
+      correct += (predicted == labels).sum().item()
+  print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+  
+  correct_pred = {classname: 0 for classname in classes}
+  total_pred = {classname: 0 for classname in classes}
+
+  with torch.no_grad():
+    for data in testloader:
+      images, labels = data
+      outputs = net(images)
+      _, predictions = torch.max(outputs, 1)
+      
+      # Collect the correct predictions for each class
+      for label, prediction in zip(labels, predictions):
+        if label == prediction:
+          correct_pred[classes[label]] += 1
+        total_pred[classes[label]] += 1
+  
+  for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count) / total_pred[classname]
+    print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
 
